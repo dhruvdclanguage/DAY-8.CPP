d01805ad077a25ba929fcbe30ff17f477e7b02e3
dhruvdclanguage <126230133+dhruvdclanguage@users.noreply.github.com>
2023-04-26 13:02:09 +0530

    Add files via upload

---
+++ day 8.5.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class Dhruv{
+	
+	public:
+		
+		Dhruv(){			
+			cout<<"SONANI DHRUV N."<<endl;	
+		}
+	
+};
+
+int main(){	
+	Dhruv obj;
+	
+	return 0 ;
+}
